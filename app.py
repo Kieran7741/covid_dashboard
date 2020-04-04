884a2edea8d5427e1825273c5eeab4c12f0dbdd4
Kieran7741 <kieran7741@gmail.com>
2020-04-04 22:25:38 +0100

    Added callbacks

--- app.py
+++ app.py
@@ -4,6 +4,64 @@ import dash_html_components as html
 import pandas as pd
 from requests import get
 from requests.exceptions import HTTPError
+from dash.dependencies import Input, Output
+
+
+def drop_down_list():
+    """
+    Build drop down with all available countries
+    :return:
+    """
+    list_of_countries = sorted(list(set(pd.read_csv('covid_19_latest.csv')['countriesAndTerritories'])))
+
+    return dcc.Dropdown(
+        id='country_list',
+        options=[{'label': country, 'value': country} for country in list_of_countries],
+        value=list_of_countries[0]
+    )
+
+
+def filter_by_country(country):
+    """Retrieve data for a country"""
+    loaded_data = pd.read_csv('covid_19_latest.csv')
+    data = loaded_data[loaded_data.countriesAndTerritories == country]
+    return data
+
+
+def graph_deaths_for_country(country):
+    data = filter_by_country(country)
+
+    return html.Div(children=[
+        html.H1(
+            id='daily_total_header',
+            children=f'Daily Deaths: {country}',
+            style={
+                'textAlign': 'center',
+                'color': 'white',
+            }
+        ),
+        drop_down_list(),
+        dcc.Graph(id='deaths_per_day',
+                  figure={
+                      'data': [{'x': list(reversed(list(data['dateRep']))),
+                                'y': list(reversed([int(count) for count in data['deaths']])), 'type': 'bar',
+                                'name': country}]
+                  })
+    ])
+
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+colors = {
+    'background': '#111111',
+    'text': '#7FDBFF'
+}
+
+app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
+    graph_deaths_for_country('Germany'),
+])
 
 
 def download_covid_dataset(file_name='covid_19_latest.csv',
@@ -25,88 +83,13 @@ def download_covid_dataset(file_name='covid_19_latest.csv',
         print(f'Failed to download dataset from {url} due to {e}')
 
 
-def filter_by_country(country):
-    """Retrieve data for a country"""
-    loaded_data = pd.read_csv('covid_19_latest.csv')
-    data = loaded_data[loaded_data.countriesAndTerritories == country]
-    return data
+def example_graph():
+    df = pd.read_csv(
+        'https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv')
 
-
-def graph_deaths_for_country(country):
-    data = filter_by_country(country)
-
-    return html.Div(children=[
-        html.H1(
-            children='Hello Dash',
-            style={
-                'textAlign': 'center',
-                'color': 'white',
-            }
-        ),
+    return html.Div([
         dcc.Graph(
-             figure={
-                 'data': [{'x': list(reversed(list(data['dateRep']))),
-                           'y': list(reversed([int(count) for count in data['deaths']])), 'type': 'bar', 'name': country}]
-             }
-            )])
-
-
-#
-# 'data': [
-#     {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-
-def create_app():
-    """
-    Create the Dash application
-    :return: Dash application
-    :rtype: `dash.Dash`
-    """
-    external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-    df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv')
-
-    app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
-    colors = {
-        'background': '#111111',
-        'text': '#7FDBFF'
-    }
-
-    app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
-        html.H1(
-            children='Hello Dash',
-            style={
-                'textAlign': 'center',
-                'color': colors['text']
-            }
-        ),
-
-        html.Div(children='Dash: A web application framework for Python.', style={
-            'textAlign': 'center',
-            'color': colors['text']
-        }),
-
-        dcc.Graph(
-            id='example-graph-2',
-            figure={
-                'data': [
-                    {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                    {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-                ],
-                'layout': {
-                    'plot_bgcolor': colors['background'],
-                    'paper_bgcolor': colors['background'],
-                    'font': {
-                        'color': colors['text']
-                    }
-                }
-            }
-        ),
-
-        graph_deaths_for_country('Poland'),
-
-    html.Div([
-        dcc.Graph(
-            id='life-exp-vs-gdp',
+            id='daily_deaths',
             figure={
                 'data': [
                     dict(
@@ -132,11 +115,20 @@ def create_app():
             }
         )
     ])
-    ])
 
-    return app
+
+@app.callback([Output('deaths_per_day', 'figure'), Output('daily_total_header', 'children')],
+              [Input('country_list', 'value')])
+def update_daily_deaths_graph(country):
+    data = filter_by_country(country)
+
+    return [{
+        'data': [{'x': list(reversed(list(data['dateRep']))),
+                  'y': list(reversed([int(count) for count in data['deaths']])), 'type': 'bar', 'name': country}]},
+        f'Daily Deaths: {country}']
 
 
 if __name__ == '__main__':
+    print('Downloading latest Covid-19 figures.')
     download_covid_dataset()
-    app = create_app().run_server(debug=True)
+    app.run_server(debug=True)
